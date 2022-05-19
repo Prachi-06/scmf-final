7810ed36e882fbe266002ee36b777189c6e02afa
Prachi-06 <101243438+Prachi-06@users.noreply.github.com>
2022-05-20 00:07:12 +0530

    Add files via upload

---
+++ constructor in inheritence.c
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class person{
+ public:
+     person(){
+     cout<<"c++";}
+
+
+     };
+     class person1:public person{
+ public:
+     person1(){
+     cout<<"java";}
+ };
+ int main(){
+ person1 obj;
+ }
+
